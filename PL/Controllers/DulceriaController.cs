04fca52c8b4846ce9f735f1b32e3b3db479e7fb4
EduSantiagoR <e.santiago.r05@gmail.com>
2023-10-23 17:29:43 -0600

    Generar pdf

--- PL/Controllers/DulceriaController.cs
+++ PL/Controllers/DulceriaController.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml.html.head;
+using iTextSharp.tool.xml;
 
 namespace PL.Controllers
 {
@@ -94,6 +99,41 @@ namespace PL.Controllers
                 carrito.Carrito.Add(objDulce);
             }
             return carrito;
+        }
+        public ActionResult GenerarPDF()
+        {
+            ML.Venta carrito = new ML.Venta();
+            carrito.Carrito = new List<object>();
+            GetCarrito(carrito);
+            string htmlContent = "<head><link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN\" crossorigin=\"anonymous\"></head>";
+
+            htmlContent += "<body><table class=\"table table-dark text-center\"><thead><tr><th>Nombre</th><th>Descripción</th><th>Cantidad</th><th>Precio</th></tr></thead><tbody>";
+            foreach (ML.Dulceria dulce in carrito.Carrito){
+                htmlContent += $"<tr><td>{dulce.Nombre}</td><td>{dulce.Descripcion}</td><td>{dulce.Cantidad}</td><td>{dulce.Precio}</td></tr>";
+            }
+            htmlContent += "</tbody></table><script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL\" crossorigin=\"anonymous\"></script></body>";
+
+            Document document = new Document();
+            document.AddTitle("Productos adquiridos");
+            
+            MemoryStream memoryStream = new MemoryStream();
+            PdfWriter writer = PdfWriter.GetInstance(document, memoryStream);
+            document.Open();
+
+            HTMLWorker htmlWorker = new HTMLWorker(document);
+            htmlWorker.Parse(new StringReader(htmlContent));
+
+            //using(TextReader reader = new StringReader(htmlContent))
+            //{
+            //    XMLWorkerHelper.GetInstance().ParseXHtml(writer,document,reader);
+            //}
+
+            document.Close();
+
+            HttpContext.Session.Clear();
+
+            return File(memoryStream.ToArray(), "application/pdf", "Compra-" + DateTime.Now + ".pdf");
+
         }
     }
 }
